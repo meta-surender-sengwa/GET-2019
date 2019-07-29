870618b31ca9b5f7f23b66d46f5f2f7ef605e0c4
Surender Sengwa <surender.sengwa@metacube.com>
2019-07-29 15:50:47 +0530

    SCF Session 7 resubmittion

--- SCF/Assignment-7/Sparse Matrix/SparseMatrix.java
+++ SCF/Assignment-7/Sparse Matrix/SparseMatrix.java
@@ -1,61 +1,41 @@
 import java.io.IOException;
 
-/**
- * This class contains methods for operations on sparse matrices
- * @author Surender
- *
- */
-public class SparseMatrix {
-	private final int[][] SparseMatrix;
-	private final int noOfRows;
-	private final int noOfColumns;
+import org.junit.Test;
 
-	/**
-	 * This parameterized is constructor used to initialize matrix in reduced form
-	 * @param reducedSparseMatrix is a 2d array containing information about nonzero element's row, column and value
-	 * @param matrixRow stores number of rows in original matrix
-	 * @param matrixColumn stores number of column in original matrix
-	 */
-	public SparseMatrix(int[][] reducedSparseMatrix, int matrixRow, int matrixColumn) {
-		this.SparseMatrix = reducedSparseMatrix;
-		this.noOfColumns = matrixRow;
-		this.noOfRows = matrixColumn;
+/**
+ * @author Surender
+ *	This class contains methods for operations on sparse matrices
+ */
+public final class SparseMatrix {
+	private int[][] matrix;
+	private int[][] sparseMatrix;
+	
+	SparseMatrix(int[][] array) {
+		this.matrix = array; 
 	}
 	
-	/**
-	 * This parameterized constructor initializes sparse matrix by storing only non zero element and their row and column
-	 * @param matrix is a 2d sparse matrix array
-	 */
-	public SparseMatrix(int matrix[][]) {
-		this.noOfRows = matrix.length;
-		this.noOfColumns = matrix[0].length;
-
-		int numberOfNonZeroElements = countNonZeroElementsInSparseMatrix(matrix);
-		this.SparseMatrix = new int[numberOfNonZeroElements][3];
-
-		int rowIndex = 0; 
-		for (int arrayRowIndex = 0; arrayRowIndex < matrix.length; arrayRowIndex ++) {
-			for (int arrayColIndex = 0; arrayColIndex < matrix[0].length; arrayColIndex ++) {
-				if (matrix[arrayRowIndex][arrayColIndex] != 0) {
-					this.SparseMatrix[rowIndex][0] = arrayRowIndex;
-					this.SparseMatrix[rowIndex][1] = arrayColIndex;
-					this.SparseMatrix[rowIndex][2] = matrix[arrayRowIndex][arrayColIndex];
-					rowIndex++;
+	private int[][] convertToRepresentation(int[][] array) {
+		int numberOfNonZeroElements = countNumberOfNonZeroElements(array);
+		int[][] sparseArray = new int[numberOfNonZeroElements][3];
+		int index = 0; 
+		for (int rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
+			for (int columnIndex = 0; columnIndex < matrix[0].length; columnIndex++) {
+				if (matrix[rowIndex][columnIndex] != 0) {
+					sparseArray[index][0] = rowIndex;
+					sparseArray[index][1] = columnIndex;
+					sparseArray[index][2] = matrix[rowIndex][columnIndex];
+					index++;
 				}
 			}
 		}
+		return sparseArray;
 	}
-
-	/**
-	 * This function counts the non zero elements in the 2d sparse matrix array
-	 * @param matrix is a 2d sparse matrix array
-	 * @return number of non zero elements in the 2d matrix array
-	 */
-	private int countNonZeroElementsInSparseMatrix(int[][] matrix) {
+	
+	private int countNumberOfNonZeroElements(int[][] array) {
 		int numberOfNonZeroElement = 0;
-		for (int arrayRowIndex = 0; arrayRowIndex < matrix.length; arrayRowIndex ++) {
-			for (int arrayColIndex = 0; arrayColIndex < matrix[0].length; arrayColIndex ++) {
-				if (matrix[arrayRowIndex][arrayColIndex] != 0) {
+		for (int rowIndex = 0; rowIndex < array.length; rowIndex++) {
+			for (int columnIndex = 0; columnIndex < array[0].length; columnIndex++) {
+				if (array[rowIndex][columnIndex] != 0) {
 					numberOfNonZeroElement++;
 				}
 			}
@@ -63,236 +43,57 @@ public class SparseMatrix {
 		return numberOfNonZeroElement;
 	}
 	
-	/**
-	 * This method converts the reduced sparse matrix array to its full size
-	 * @return  full size sparse matrix array containing zero element and non zero elements
-	 */
-	public int[][] getMatrix() {
-		int matrix[][] = new int[this.noOfRows][this.noOfColumns];
-		
-		for (int arrayRowIndex = 0; arrayRowIndex < this.noOfRows; arrayRowIndex ++) {
-			for (int arrayColumnIndex = 0; arrayColumnIndex < this.noOfColumns; arrayColumnIndex++) {
-				boolean elementFound = false;
-				for(int rowIndex =0; rowIndex < this.SparseMatrix.length; rowIndex++) {
-					if (this.SparseMatrix[rowIndex][0] == arrayRowIndex && this.SparseMatrix[rowIndex][1] == arrayColumnIndex) {
-						elementFound = true;
-						matrix[arrayRowIndex][arrayColumnIndex] = this.SparseMatrix[rowIndex][2];
-						rowIndex ++;
-						break;
-					}
-				}
-					if(!elementFound){
-						matrix[arrayRowIndex][arrayColumnIndex] = 0;
-					}
-			}
+	public int[][] getTransposeMatrix() {
+		this.sparseMatrix = convertToRepresentation(this.matrix);;;
+		int[][] transposeMatrix = new int[sparseMatrix.length][3];
+		for(int index = 0; index < sparseMatrix.length; index++) {
+			transposeMatrix[index][0] = sparseMatrix[index][1];
+			transposeMatrix[index][1] = sparseMatrix[index][0];
+			transposeMatrix[index][2] = sparseMatrix[index][2];
 		}
-		return matrix;
+		return transposeMatrix;
 	}
-	
-	/**
-	 * This method sorts the 2d array containing only non zero element according to their stored row and column value
-	 * @param matrix is 2d array containing row, column and value of non zero element
-	 * @return sorted 2d array containing row, column and value of non zero element
-	 */
-	private int[][] sortMatrixAccordingToRowColumn(int[][] matrix) {
-		for(int firstRowIndex =0; firstRowIndex < matrix.length; firstRowIndex ++) {
-			for(int secondRowIndex = (firstRowIndex+1); secondRowIndex < matrix.length; secondRowIndex ++ ) {
-				
-				if(matrix[firstRowIndex][0] > matrix[secondRowIndex][0]) {
-					int tempData = matrix[secondRowIndex][0];
-					matrix[secondRowIndex][0] = matrix[firstRowIndex][0];
-					matrix[firstRowIndex][0] = tempData;
-					
-					tempData = matrix[secondRowIndex][1];
-					matrix[secondRowIndex][1] = matrix[firstRowIndex][1];
-					matrix[firstRowIndex][1] = tempData;
-					
-					tempData = matrix[secondRowIndex][2];
-					matrix[secondRowIndex][2] = matrix[firstRowIndex][2];
-					matrix[firstRowIndex][2] = tempData;
-				}
-			}
-		}
-		
-		for(int firstRowIndex = 0; firstRowIndex < matrix.length; firstRowIndex ++) {
-			for(int secondRowIndex = (firstRowIndex+1); secondRowIndex < matrix.length; secondRowIndex ++ ) {
-				
-				if(matrix[firstRowIndex][0] == matrix[secondRowIndex][0]) {
-					if(matrix[firstRowIndex][1] > matrix[secondRowIndex][1]) {
-						int tempData = matrix[secondRowIndex][1];
-						
-						matrix[secondRowIndex][1] = matrix[firstRowIndex][1];
-						matrix[firstRowIndex][1] = tempData;
-						
-						tempData = matrix[secondRowIndex][2];
-						matrix[secondRowIndex][2] = matrix[firstRowIndex][2];
-						matrix[firstRowIndex][2] = tempData;
-					}
-				}
-			}
-		}
-		return matrix;
-	}
-	
-	/**
-	 * This method is used to find the transpose of a sparse matrix
-	 * @return transpose of the sparse matrix 
-	 */
-	public SparseMatrix getTransposeOfSparseMatrix() {
-		int[][] transposeMatrix = new int[this.SparseMatrix.length][3];
-		for(int rowIndex = 0; rowIndex < this.SparseMatrix.length; rowIndex ++) {
-			transposeMatrix[rowIndex][0] = this.SparseMatrix[rowIndex][1];
-			transposeMatrix[rowIndex][1] = this.SparseMatrix[rowIndex][0];
-			transposeMatrix[rowIndex][2] = this.SparseMatrix[rowIndex][2];
-		}
-		transposeMatrix = sortMatrixAccordingToRowColumn(transposeMatrix);
-		return new SparseMatrix(transposeMatrix, this.noOfColumns, this.noOfRows);
-	}
-	
-	/**
-	 * This method is used to check if the given matrix is symmetrical or not
-	 * @return true if matrix is symmetrical else return false
-	 */
-	public boolean isSparseMatrixSymmetrical(){
-		SparseMatrix transposeMatrix = this.getTransposeOfSparseMatrix();
-		int counter = 0;
-		for(int firstIndex=0; firstIndex < this.SparseMatrix.length; firstIndex++) {
-			for(int secondIndex=0; secondIndex < transposeMatrix.SparseMatrix.length; secondIndex++) {
-				
-				if(this.SparseMatrix[firstIndex][0] == transposeMatrix.SparseMatrix[secondIndex][0] 
-						&& this.SparseMatrix[firstIndex][1] == transposeMatrix.SparseMatrix[secondIndex][1] 
-								&& this.SparseMatrix[firstIndex][2] == transposeMatrix.SparseMatrix[secondIndex][2]) {
-					counter ++;
-				}
-			}
-		}
-		if(counter == this.SparseMatrix.length) {
-			return true;
-		}
-		return false;
-	}
-	
-	/**
-	 * This method is used to add two sparse matrices
-	 * @param matrix is an object of SparseMatrix class containing only non zero element
-	 * @return object of SparseMatrix containing only non zero element
-	 * @throws IOException throws IO-exception when the size of the two matrices is different
-	 */
-	public SparseMatrix getAdditionOfTwoSparseMatrix(SparseMatrix matrix) throws IOException {
-		if(this.noOfRows != matrix.noOfRows || this.noOfColumns != matrix.noOfColumns) {
-			throw new IOException("Addition is not possible");
-		}
-		
-		int sameElementCount = 0;
-		for(int firstIndex = 0; firstIndex < this.SparseMatrix.length; firstIndex ++) {
-			boolean elementFound = false;
-			for(int secondIndex =0; secondIndex < matrix.SparseMatrix.length; secondIndex ++) {
-				
-				if(this.SparseMatrix[firstIndex][0] == matrix.SparseMatrix[secondIndex][0]) {
-					if(this.SparseMatrix[firstIndex][1] == matrix.SparseMatrix[secondIndex][1]) {
-						sameElementCount ++;
-					}
+
+	public boolean checkSymmetryOfMatrix() {
+		for (int indexI = 0; indexI < this.matrix.length; indexI++) {
+			for (int indexJ = 0; indexJ < this.matrix[0].length; indexJ++) {
+				if (this.matrix[indexI][indexJ] != this.matrix[indexJ][indexI]) {
+					return false;
 				}
 			}
 			
 		}
-		int[][] sumMatrix = new int[ (this.SparseMatrix.length + matrix.SparseMatrix.length) - sameElementCount ][3];
-		int rowIndex = 0;
-		for(int firstIndex = 0; firstIndex < this.SparseMatrix.length;firstIndex++) {
-			boolean elementFound = false;
-			for(int secondIndex =0; secondIndex < matrix.SparseMatrix.length; secondIndex ++) {
-				
-				if(this.SparseMatrix[firstIndex][0] == matrix.SparseMatrix[secondIndex][0]) {
-					if(this.SparseMatrix[firstIndex][1] == matrix.SparseMatrix[secondIndex][1]) {
-						sumMatrix[rowIndex][0] = this.SparseMatrix[firstIndex][0];
-						sumMatrix[rowIndex][1] = this.SparseMatrix[firstIndex][1];
-						sumMatrix[rowIndex][2] = this.SparseMatrix[firstIndex][2] + matrix.SparseMatrix[secondIndex][2];
-						rowIndex ++;
-						elementFound = true;
-					}
-				}
-			}
-			if(!elementFound) {
-				sumMatrix[rowIndex][0] = this.SparseMatrix[firstIndex][0];
-				sumMatrix[rowIndex][1] = this.SparseMatrix[firstIndex][1];
-				sumMatrix[rowIndex][2] = this.SparseMatrix[firstIndex][2]; 
-				rowIndex ++;
-			}
-		}
-		
-		for(int firstIndex = 0; firstIndex < matrix.SparseMatrix.length; firstIndex ++) {
-			boolean elementFound = false;
-			for(int secondIndex =0; secondIndex < this.SparseMatrix.length; secondIndex++) {
-				if(matrix.SparseMatrix[firstIndex][0] == this.SparseMatrix[secondIndex][0]) {
-					if(matrix.SparseMatrix[firstIndex][1] == this.SparseMatrix[secondIndex][1]) {
-						elementFound = true;
-					}
-				}
-			}
-			if(!elementFound) {
-				sumMatrix[rowIndex][0] = matrix.SparseMatrix[firstIndex][0];
-				sumMatrix[rowIndex][1] = matrix.SparseMatrix[firstIndex][1];
-				sumMatrix[rowIndex][2] = matrix.SparseMatrix[firstIndex][2]; 
-				rowIndex++;
-			}
-		}
-		return new SparseMatrix(sumMatrix, this.noOfRows, this.noOfColumns);
-		
+		return true;
 	}
-
-
-	/**
-	 * This method is used to multiply two sparse matrices
-	 * @param matrix is an object of SparseMatrix class containing only non zero element
-	 * @return multiplication result of the two Sparse Matrices
-	 * @throws IOException throws IO-exception when no of columns of first matrix is not equal to the no of rows of second matrix
-	 */
-	public SparseMatrix getMultiplicationOfTwoSparseMatrices(SparseMatrix matrix) throws IOException {
-		if(this.noOfColumns != matrix.noOfRows){
+	
+	public int[][] getSumOfMatrices(int[][] array2) throws IOException {
+		if(this.matrix.length != array2.length || this.matrix[0].length != array2[0].length) {
+			throw new IOException("Addition is not possible");
+		}
+		int[][] sumOfMatrices = new int[this.matrix.length][this.matrix[0].length];
+		
+		for (int indexI = 0; indexI < sumOfMatrices.length; indexI++) {
+			for (int indexJ = 0; indexJ < sumOfMatrices[0].length; indexJ++) {
+				sumOfMatrices[indexI][indexJ] = this.matrix[indexI][indexJ] + array2[indexI][indexJ];
+			}
+		}
+		return sumOfMatrices;
+	}
+	
+	public int[][] getMultiplicationOfMatrices(int[][] array2) throws IOException {
+		if(this.matrix[0].length != array2.length){
 			throw new IOException("Multiplication is not possible");
 		}
+		int[][] multiplicationOfMatrices = new int[this.matrix[0].length][array2.length];
 		
-		int[][] multiplicationMatrix = new int[this.noOfRows][matrix.noOfColumns];
-		int sum = 0, firstMatrixElement = 0, secondMatrixElement = 0;
-		boolean firstMatrixElementFound = false, secondMatrixElementFound = false;
-	
-		for (int firstMatricRowIndex = 0; firstMatricRowIndex < this.noOfRows; firstMatricRowIndex ++) {
-			for (int secondMatrixColumnIndex = 0; secondMatrixColumnIndex < matrix.noOfColumns; secondMatrixColumnIndex ++) {
-				for (int firstMatrixColumnIndex = 0; firstMatrixColumnIndex < this.noOfColumns; firstMatrixColumnIndex ++) {
-					
-					for (int rowIndex = 0; rowIndex < this.SparseMatrix.length; rowIndex ++) {
-						
-						if (this.SparseMatrix[rowIndex][0] == firstMatricRowIndex && this.SparseMatrix[rowIndex][1] == firstMatrixColumnIndex) {
-							firstMatrixElement = this.SparseMatrix[rowIndex][2];
-							firstMatrixElementFound = true;
-							break;
-						}
-					}
-					for (int rowIndex = 0; rowIndex < matrix.SparseMatrix.length; rowIndex ++) {
-						if (matrix.SparseMatrix[rowIndex][0] == firstMatrixColumnIndex && matrix.SparseMatrix[rowIndex][1] == secondMatrixColumnIndex) {
-							secondMatrixElement = matrix.SparseMatrix[rowIndex][2];
-							secondMatrixElementFound = true;
-							break;
-						}
-					}
-					if (!firstMatrixElementFound) {
-						firstMatrixElement = 0;
-					}
-					if (!secondMatrixElementFound) {
-						secondMatrixElement = 0;
-					}
-					sum = sum + firstMatrixElement * secondMatrixElement;
-					firstMatrixElementFound = false;
-					secondMatrixElementFound = false;
+		for (int indexI = 0; indexI < this.matrix.length; indexI++) {
+			for (int indexJ = 0; indexJ < array2[0].length; indexJ++) {
+				for (int indexK = 0; indexK < this.matrix[0].length; indexK++) {
+					multiplicationOfMatrices[indexI][indexJ] = multiplicationOfMatrices[indexI][indexJ] +
+							this.matrix[indexI][indexK] * array2[indexK][indexJ];
 				}
-
-				multiplicationMatrix[firstMatricRowIndex][secondMatrixColumnIndex] = sum;
-				sum = 0;
 			}
 		}
-		return new SparseMatrix(multiplicationMatrix);
+		return multiplicationOfMatrices;
 	}
-}	
-	
-	
-	
+}
--- SCF/Assignment-7/Sparse Matrix/TestSparseMatrix.java
+++ SCF/Assignment-7/Sparse Matrix/TestSparseMatrix.java
@@ -1,66 +1,44 @@
 import static org.junit.Assert.*;
 import java.io.IOException;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.hamcrest.*;
 
 public class TestSparseMatrix {
+	int[][] array = new int[][] {{0,0,1},{0,2,0},{1,0,3}};
+	SparseMatrix sparseMatrix = new SparseMatrix(array);
 	
 	@Test
-	public void testGetTransposeOfSparseMatrix() {
-		int[][] array1 = new int[][] {{0,0,1},{0,0,2},{0,0,3}};
-		SparseMatrix matrix1 = new SparseMatrix(array1);
-		int[][] expectedTranspose = new int[][] {{0,0,0},{0,0,0},{1,2,3}};
-		int[][] actualTranspose = matrix1.getTransposeOfSparseMatrix().getMatrix();
-		assertArrayEquals(expectedTranspose,actualTranspose);
-
-		int[][] array2 = new int[][] {{1,0,0},{2,0,0},{3,0,0}};
-		SparseMatrix matrix2 = new SparseMatrix(array2);
-		expectedTranspose = new int[][] {{1,2,3},{0,0,0},{0,0,0}};
-		actualTranspose = matrix2.getTransposeOfSparseMatrix().getMatrix();
-		assertArrayEquals(expectedTranspose,actualTranspose);
+	public void testGetTransposeMatrix() {
+		int[][] expectedTranspose = new int[][] {{2,0,1},{1,1,2},{0,2,1},{2,2,3}};
+		int[][] actualTranspose = sparseMatrix.getTransposeMatrix();
+		assertArrayEquals(expectedTranspose, actualTranspose);
 	}
 	
 	@Test
-	public void testIsSparseMatrixSymmetrical() {
-		int[][] array1 = new int[][] {{0,0,1},{0,2,0},{1,0,3}};
-		SparseMatrix matrix = new SparseMatrix(array1);
-		assertTrue(matrix.isSparseMatrixSymmetrical());
-		
-		int[][] array2 = new int[][] {{1,0,0},{2,0,0},{3,0,0}};
-		matrix = new SparseMatrix(array2);
-		assertFalse(matrix.isSparseMatrixSymmetrical());
-		
-		int[][] array3 = new int[][] {{1,0,0},{0,1,0},{0,0,1}};
-		matrix = new SparseMatrix(array3);
-		assertTrue(matrix.isSparseMatrixSymmetrical());
+	public void testCheckSymmetryOfMatrix() {
+		assertTrue(sparseMatrix.checkSymmetryOfMatrix());
 	}
 	
 	@Test
-	public void testGetAdditionOfTwoSparseMatrices() {
-		int[][] array1 = new int[][] {{0,0,1},{0,2,0},{1,0,3}};
-		SparseMatrix matrix1 = new SparseMatrix(array1);		
+	public void testGetSumOfMatrices() {
 		int[][] array2 = new int[][] {{1,0,0},{2,0,0},{3,0,0}};
-		SparseMatrix matrix2 = new SparseMatrix(array2);
 		int[][] expectedAddition = new int[][] {{1,0,1},{2,2,0},{4,0,3}};
 		
 		try {
-			int[][] actualAddition = matrix1.getAdditionOfTwoSparseMatrix(matrix2).getMatrix();
+			int[][] actualAddition = sparseMatrix.getSumOfMatrices(array2);
 			assertArrayEquals(expectedAddition, actualAddition);
 		} catch (IOException Ex) {
 			Ex.printStackTrace();
 		}
 	}
-
+	
 	@Test
-	public void testGetMultiplicationOfTwoSparseMatrix() {
-		int[][] array1 = new int[][] {{0,0,1},{0,2,0},{1,0,3}};
-		SparseMatrix matrix1 = new SparseMatrix(array1);		
+	public void testGetMultiplicationOfMatrices() {
 		int[][] array2 = new int[][] {{1,0,0},{2,0,0},{3,0,0}};
-		SparseMatrix matrix2 = new SparseMatrix(array2);
-		int[][] expectedMultiplication = new int[][] {{3,0,0},{4,0,0},{10,0,0}};
+		int[][] expectedMultiplication= new int[][] {{3,0,0},{4,0,0},{10,0,0}};
 		
 		try {
-			int[][] actualMultiplication = matrix1.getMultiplicationOfTwoSparseMatrices(matrix2).getMatrix();
+			int[][] actualMultiplication = sparseMatrix.getMultiplicationOfMatrices(array2);
 			assertArrayEquals(expectedMultiplication, actualMultiplication);
 		} catch (IOException Ex) {
 			Ex.printStackTrace();
