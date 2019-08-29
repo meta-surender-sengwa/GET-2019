d7de61606a8726eb623d985ba301f2cbfc3c4198
Surender Sengwa <surender.sengwa@metacube.com>
2019-08-29 17:30:51 +0530

    EAD Assignment 1

--- EAD/Assignment-1/Parking.js
+++ EAD/Assignment-1/Parking.js
@@ -1,88 +1,22 @@
-function validation() {
+function validateUserDetails() {
+    var userRegisterationForm = document.forms.RegForm;
 
-    var name = document.forms["RegForm"]["Name"];
-    var email = document.forms["RegForm"]["Email"];
-    var phone = document.forms["RegForm"]["Contact"];
-    var password = document.forms["RegForm"]["Password"];
-    var Confirm = document.forms["RegForm"]["conPassword"];
-    var regex = new RegExp("\\d");
-    var filtercontact = new RegExp('([a-zA-Z!@#$%^&*()_+])');
-    var filterPassword = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    var name = userRegisterationForm.Name;
+    var email = userRegisterationForm.Email;
+    var phone = userRegisterationForm.Contact;
+    var password = userRegisterationForm.Password;
+    var Confirm = userRegisterationForm.Conpassword;
 
-    if (password.value.length < 8) {
-        window.alert("Password should be minimum 8 character");
-        password.focus();
-        return false;
-    }
-    if(name.value.length <= 2) {
-        window.alert("Name should be greater than 2 chracter");
-        name.focus();
-        return false;
-        
-    }
-    if (filterPassword.test(password)){
-        window.alert("Insert Valid password format eg Abc@123");
-        password.focus();
-        return false;
+    var nameRegEx = /^[a-zA-Z]+[\s]{1}[a-zA-Z]+$/i;
 
-    }
-    if (phone.value.length != 10 ) {
-        window.alert("Phone number should be of 10 digit");
-        phone.focus();
-        return false;
-    }
-    if (name.value == "") {
-        window.alert("Please enter your name.");
-        name.focus();
-        return false;
-    } if (regex.test(name.value)) {
-        window.alert("Please dont enter numeric in name");
-        name.focus();
-        return false;
-    }
-    if (filtercontact.test(phone.value)) {
-        window.alert("Please enter only numeric in contacts");
+    if (nameRegEx.test(name.value)) {
+        window.alert("Please enter your full name");
         phone.focus();
         return false;
     }
 
-    if (email.value == "") {
-        window.alert("Please enter a valid e-mail address.");
-        email.focus();
-        return false;
-    }
-
-    if (email.value.indexOf("@", 0) < 0) {
-        window.alert("Please enter a valid e-mail address.");
-        email.focus();
-        return false;
-    }
-
-    if (email.value.indexOf(".", 0) < 0) {
-        window.alert("Please enter a valid e-mail address.");
-        email.focus();
-        return false;
-    }
-
-    if (phone.value == "") {
-        window.alert("Please enter your telephone number.");
-        phone.focus();
-        return false;
-    }
-
-    if (password.value == "") {
-        window.alert("Please enter your password");
-        password.focus();
-        return false;
-    }
-    if (Confirm.value == "") {
-        window.alert("Please enter your password");
-        password.focus();
-        return false;
-    }
-
-    var n = password.value.localeCompare(Confirm.value);
-    if( n != 0){
+    var count = password.value.localeCompare(Confirm.value);
+    if( count != 0){
         window.alert("Password and confirm password do not match");
         password.focus();
         return false;
@@ -91,35 +25,42 @@ function validation() {
     return true;
 }
 
-function collapse() {
-    var flag = validation();
-    if(flag == true) {
-        window.alert("Thank you for registration!! Your registration number is 123346");
-    document.getElementById("application").innerHTML = "";
+function registerUser() {
+    var flag = validateUserDetails();
+    if(flag) {
+        var registerationId = "meta123";
+        window.alert("Thank you for registration!! Your registration number is " + registerationId);
+        document.getElementById("application").innerHTML = "";
+        document.getElementById("vehicle").style.display = "block";
     }
-    document.getElementById("vehicle").style.display = "inherit";
 }
+		
+function validateVehicleDetails(){
+    var name = document.forms.vehicleform.vname;
+    var number = document.forms.vehicleform.number;
 
-function validation2(){
-    var name = document.forms["vehicleform"]["vname"];
-    var vnumber = document.forms["vehicleform"]["number"];
-    if (name.value == "") {
-        window.alert("Please enter vehicle name");
-        name.focus();
+    var nameRegEx = /^[a-zA-Z0-9]*$/i;
+    var numberRegEx = /^[A-Z]{2}[0-9]{2}[A-Z]{2}[0-9]{4}$/;
+
+    if (nameRegEx.test(name.value)) {
+        window.alert("Please enter proper vehicle name");
+        phone.focus();
         return false;
     }
-    if (vnumber.value == "") {
-        window.alert("Please enter vehicle number");
-        vnumber.focus();
+
+    if (numberRegEx.test(number.value)) {
+        window.alert("Please enter proper vehicle number");
+        phone.focus();
         return false;
     }
-    
+
     return true;
 }
 
-function price() {
-    var flag = validation2();
-    if(flag == true) {
+function registerVehicle() {
+    var flag = validateVehicleDetails();
+
+    if(flag) {
         document.getElementById("curr_type").style.display= "block";
         var type = document.getElementById("vehicle_type").value;
         document.getElementById("vehicle").style.display ="none";
@@ -127,59 +68,55 @@ function price() {
         if (type == "cycle") {
             document.getElementById("cycle").style.display = "block";
         } else if (type == "bike") {
-            document.getElementById("bike").style.display = "block";
+           document.getElementById("bike").style.display = "block";
         } else {
-            document.getElementById("car").style.display = "block";
+          document.getElementById("car").style.display = "block";
         }
         document.getElementById("bill").style.display = "block";
     }
 }
 
-function bill_cal() {
-
+function calculateBill() {
     var ve_type = document.getElementById("vehicle_type").value;
     var ctype = document.getElementById("currecy_type").value;
     var ele = document.getElementsByName(ve_type);
     for (i = 0; i < ele.length; i++) {
-        if (ele[i].checked) {
+        if (ele[i].checked)
             document.getElementById("show_price").innerHTML= "Total bill in USD : " + ele[i].value*0.014;
-        }
     }
 }
-    
 
-function change_currency() {
+function changeCurrency() {
     var ve_type = document.getElementById("vehicle_type").value;
     var c_type = document.getElementById("currecy_type").value;
 
-    switch(c_type) {
+    switch(c_type){
         case "INR":
         var a = document.getElementById(ve_type).querySelectorAll(".p1");
         for ( var index = 0; index < a.length; index++) {	
             var element = a[index].innerHTML;
             element = element*1;
-            a[index].innerHTML=element;
-            
+            a[index].innerHTML=element;        
         }
         break;
 
-        case "USD":
+        case "USD":    
         var a = document.getElementById(ve_type).querySelectorAll(".p1");
         for ( var index = 0; index < a.length; index++) {	
             var element = a[index].innerHTML;
-            element = element*0.014;
-            a[index].innerHTML=element;
-            
+            element = element * 0.014;
+            a[index].innerHTML=element;          
         }
         break;
 
         case "YEN":
         var a = document.getElementById(ve_type).querySelectorAll(".p1");
         for ( var index = 0; index < a.length; index++) {
+        
             var element = a[index].innerHTML;
             element = element*1.48;
-            a[index].innerHTML=element;  
+            a[index].innerHTML=element;
         }
         break;
-    }
+    }   
 }
