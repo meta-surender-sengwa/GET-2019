f4fe6cd97da9f9b2d99467cbd9af47ce31deadeb
Surender Sengwa <surender.sengwa@metacube.com>
2019-09-02 14:59:40 +0530

    EAD Assignment1 re

--- EAD/Assignment-1/Parking.js
+++ EAD/Assignment-1/Parking.js
@@ -7,18 +7,32 @@ function validateUserDetails() {
     var password = userRegisterationForm.Password;
     var Confirm = userRegisterationForm.Conpassword;
 
-    var nameRegEx = /^[a-zA-Z]+[\s]{1}[a-zA-Z]+$/i;
-
-    if (nameRegEx.test(name.value)) {
+    var nameRegEx = /^[a-zA-Z0-9]+[\s]{1}[a-zA-Z0-9]+$/i;
+    var emailRegEx = /^[a-zA-Z0-9]+[@]{1}[a-zA-Z]+[\.]{1}[a-zA-Z]+$/i;
+    var passwordRegEx = /^[a-zA-Z0-9]{8,20}$/i;
+    
+    if (!nameRegEx.test(name.value)) {
         window.alert("Please enter your full name");
-        phone.focus();
+        name.focus();
+        return false;
+    }
+
+    if (!emailRegEx.test(email.value)) {
+        window.alert("Please enter valid email address");
+        email.focus();
+        return false;
+    }
+
+    if (!passwordRegEx.test(password.value)) {
+        window.alert("Please enter a proper password");
+        password.focus();
         return false;
     }
 
     var count = password.value.localeCompare(Confirm.value);
-    if( count != 0){
+    if(count != 0){
         window.alert("Password and confirm password do not match");
-        password.focus();
+        Conpassword.focus();
         return false;
     }
 
@@ -27,6 +41,7 @@ function validateUserDetails() {
 
 function registerUser() {
     var flag = validateUserDetails();
+
     if(flag) {
         var registerationId = "meta123";
         window.alert("Thank you for registration!! Your registration number is " + registerationId);
@@ -39,16 +54,16 @@ function validateVehicleDetails(){
     var name = document.forms.vehicleform.vname;
     var number = document.forms.vehicleform.number;
 
-    var nameRegEx = /^[a-zA-Z0-9]*$/i;
-    var numberRegEx = /^[A-Z]{2}[0-9]{2}[A-Z]{2}[0-9]{4}$/;
+    var nameRegEx = /^[a-zA-Z0-9\s]+$/i;
+    var numberRegEx = /^[a-zA-Z]{2}[0-9]{2}[a-zA-Z]{2}[0-9]{4}$/;
 
-    if (nameRegEx.test(name.value)) {
+    if (!nameRegEx.test(name.value)) {
         window.alert("Please enter proper vehicle name");
         phone.focus();
         return false;
     }
 
-    if (numberRegEx.test(number.value)) {
+    if (!numberRegEx.test(number.value)) {
         window.alert("Please enter proper vehicle number");
         phone.focus();
         return false;
@@ -65,57 +80,102 @@ function registerVehicle() {
         var type = document.getElementById("vehicle_type").value;
         document.getElementById("vehicle").style.display ="none";
         document.getElementById("btns").style.display = "block";
+
         if (type == "cycle") {
             document.getElementById("cycle").style.display = "block";
+
         } else if (type == "bike") {
            document.getElementById("bike").style.display = "block";
+
         } else {
           document.getElementById("car").style.display = "block";
+
         }
         document.getElementById("bill").style.display = "block";
     }
 }
 
 function calculateBill() {
-    var ve_type = document.getElementById("vehicle_type").value;
-    var ctype = document.getElementById("currecy_type").value;
-    var ele = document.getElementsByName(ve_type);
-    for (i = 0; i < ele.length; i++) {
-        if (ele[i].checked)
-            document.getElementById("show_price").innerHTML= "Total bill in USD : " + ele[i].value*0.014;
+    const convertToUSD = 0.014;
+    var vehicleType = document.getElementById("vehicle_type").value;
+    var ele = document.getElementsByName(vehicleType);
+
+    for (index = 0; index < ele.length; index++) {
+        if (ele[index].checked)
+            document.getElementById("show_price").innerHTML= "Total bill in USD : " + ele[index].value * convertToUSD;
     }
 }
 
 function changeCurrency() {
-    var ve_type = document.getElementById("vehicle_type").value;
-    var c_type = document.getElementById("currecy_type").value;
+    const convertToUSD = 0.014;
+    const convertToYEN = 1.48;
 
-    switch(c_type){
-        case "INR":
-        var a = document.getElementById(ve_type).querySelectorAll(".p1");
-        for ( var index = 0; index < a.length; index++) {	
-            var element = a[index].innerHTML;
-            element = element*1;
-            a[index].innerHTML=element;        
+    var vehicleType = document.getElementById("vehicle_type").value;
+    var currencyType = document.getElementById("currecy_type").value;
+
+    var priceInINR = [[5,100,500], [10,200,1000], [20,500,3500]];
+    var row;
+
+    if(vehicleType == "cycle") {
+        row = 0;
+    } else if(vehicleType == "bike") {
+        row = 1;
+    } else if(vehicleType == "car") {
+        row = 2;
+    }
+
+    switch(currencyType){      
+    case "INR":
+        if(vehicleType=="cycle") { 
+            document.getElementById("dailyCycle").innerHTML = priceInINR[row][0];
+            document.getElementById("monthlyCycle").innerHTML = priceInINR[row][1];
+            document.getElementById("yearlyCycle").innerHTML = priceInINR[row][2];
+
+        } else if(vehicleType=="bike") { 
+            document.getElementById("dailyBike").innerHTML = priceInINR[row][0];
+            document.getElementById("monthlyBike").innerHTML = priceInINR[row][1]
+            document.getElementById("yearlyBike").innerHTML = priceInINR[row][2];
+
+        } else if(vehicleType=="car") { 
+            document.getElementById("dailyCar").innerHTML = priceInINR[row][0];
+            document.getElementById("monthlyCar").innerHTML = priceInINR[row][1];
+            document.getElementById("yearlyCar").innerHTML = priceInINR[row][2];
         }
         break;
 
-        case "USD":    
-        var a = document.getElementById(ve_type).querySelectorAll(".p1");
-        for ( var index = 0; index < a.length; index++) {	
-            var element = a[index].innerHTML;
-            element = element * 0.014;
-            a[index].innerHTML=element;          
+    case "USD":   
+        if(vehicleType=="cycle") { 
+            document.getElementById("dailyCycle").innerHTML = priceInINR[row][0] * convertToUSD;
+            document.getElementById("monthlyCycle").innerHTML = priceInINR[row][1] * convertToUSD;
+            document.getElementById("yearlyCycle").innerHTML = priceInINR[row][2] * convertToUSD;
+
+        } else if(vehicleType=="bike") { 
+            document.getElementById("dailyBike").innerHTML = priceInINR[row][0] * convertToUSD;
+            document.getElementById("monthlyBike").innerHTML = priceInINR[row][1] * convertToUSD;
+            document.getElementById("yearlyBike").innerHTML = priceInINR[row][2] * convertToUSD;
+
+        } else if(vehicleType=="car") { 
+            document.getElementById("dailyCar").innerHTML = priceInINR[row][0] * convertToUSD;
+            document.getElementById("monthlyCar").innerHTML = priceInINR[row][1] * convertToUSD;
+            document.getElementById("yearlyCar").innerHTML = priceInINR[row][2] * convertToUSD;
         }
         break;
 
-        case "YEN":
-        var a = document.getElementById(ve_type).querySelectorAll(".p1");
-        for ( var index = 0; index < a.length; index++) {
-        
-            var element = a[index].innerHTML;
-            element = element*1.48;
-            a[index].innerHTML=element;
+    case "YEN":
+        if(vehicleType=="cycle") { 
+            document.getElementById("dailyCycle").innerHTML = priceInINR[row][0] * convertToYEN;
+            document.getElementById("monthlyCycle").innerHTML = priceInINR[row][1] * convertToYEN;
+            document.getElementById("yearlyCycle").innerHTML = priceInINR[row][2] * convertToYEN;
+
+        } else if(vehicleType=="bike") { 
+            document.getElementById("dailyBike").innerHTML = priceInINR[row][0] * convertToYEN;
+            document.getElementById("monthlyBike").innerHTML = priceInINR[row][1] * convertToYEN;
+            document.getElementById("yearlyBike").innerHTML = priceInINR[row][2] * convertToYEN;
+
+        } else if(vehicleType=="car") { 
+            document.getElementById("dailyCar").innerHTML = priceInINR[row][0] * convertToYEN;
+            document.getElementById("monthlyCar").innerHTML = priceInINR[row][1] * convertToYEN;
+            document.getElementById("yearlyCar").innerHTML = priceInINR[row][2] * convertToYEN;
         }
         break;
     }   
