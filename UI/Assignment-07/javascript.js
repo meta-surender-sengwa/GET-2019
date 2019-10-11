b3f48d94fafdb49d3d9f23581f80f4b5dc6d2d4e
Surender Sengwa <surender.sengwa@metacube.com>
2019-10-11 15:43:26 +0530

    UI Assignment 07

--- UI/Assignment-07/javascript.js
+++ UI/Assignment-07/javascript.js
@@ -1,7 +1,16 @@
 var employeeName, employeeGender, employeeEmail, employeePassword, employeeMatchPassword, employeeNumber;
-
 var currentField;
 var currentForm;
+var vehicleType;
+
+var idArray = [];
+
+var employeeFormBackgroundColour = "#7FDBFF";
+var vehicleFormBackgroundColour = "#FF851B";
+var planFormBackgroundColour = "#DDDDDD";
+
+var convertToUSD = 71.74;
+var convertToYEN = 0.68;
 
 /* onclick of Register Here shows name input */
 function registerationForm() {
@@ -9,10 +18,13 @@ function registerationForm() {
     document.getElementById("navigation").style.display="none";
     document.getElementById("cover-img").style.display = "none";
 
-    document.body.style.backgroundColor = "#7FDBFF";
+    document.body.style.backgroundColor = employeeFormBackgroundColour;
 
     document.getElementById("label").style.display = "block";
     document.getElementById("employee-name").style.display = "block";
+
+    document.getElementById("button-next").style.display = "block";
+    document.getElementById("button-back").style.display = "block";
 
     document.getElementById("emp-name").focus();
 
@@ -20,10 +32,16 @@ function registerationForm() {
     currentForm = 1;
 }
 
+function backToPrevious() {
+    document.getElementById(idArr[idArr.length - 1]).style.display = "none";
+    idArray.pop()
+    document.getElementById(idArr[idArr.length - 1]).style.display = "block";
+}
+
+
 function validatePassword() {
 
     var employeePassword = document.getElementById("emp-pass").value;
-    
     var strongPassword = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
 
     if (employeePassword.length < 8) {
@@ -42,20 +60,7 @@ function validatePassword() {
     }
 }
 
-/* event listener for enter key */
-window.addEventListener('keypress', function (e) {
-    if (e.keyCode == 13) {
-        onBlur();
-    }
-}, false);
-
-
-function onBlur() {
-
-    if (currentForm == 2) {
-        onBlurForm2();
-        return false;
-    }
+function onBlurForm1() {
 
     var label = document.getElementById("label-text");
 
@@ -123,8 +128,6 @@ function onBlur() {
             break;
 
         case "password":
-
-                var employeePassword = document.getElementById("emp-pass").value;	
             
                 label.innerHTML = "Lets confirm your password";
     
@@ -136,6 +139,7 @@ function onBlur() {
                 break;
 
         case "confirm":
+            
             var employeePassword = document.getElementById("emp-pass").value;
             var employeeMatchPassword = document.getElementById("confirm-pass").value;
 
@@ -147,16 +151,196 @@ function onBlur() {
                 document.getElementById("error").innerHTML = " ";
             }
 
-            label.innerHTML = "Please enter your contact number  ...";
+            label.innerHTML = "please enter your contact number  ...";
 
             document.getElementById("confirm-password").style.display = "none";
             document.getElementById("employee-contact").style.display = "block";
 
-            document.getElementById("emp-contact").focus();
             document.getElementById("next1").style.display = "block";
+            document.getElementById("button-next").style.display = "none";
+            
+            document.getElementById("emp-contact").focus();
             currentField = "contact";
 
             break;
     }
 }
 
+
+
+/*  shows first input of vehcle form */
+function vehicleForm() {
+
+    var employeeContact = document.getElementById("emp-contact").value;
+
+    if (!(/^[0-9]{10}$/.test(employeeContact))) {
+        document.getElementById("error").innerHTML = "please enter a valid 10 digit numeric contact number";
+        return false;
+        
+    } else {
+        document.getElementById("error").innerHTML = " ";
+    }
+
+    document.body.style.backgroundColor = vehicleFormBackgroundColour;
+    label.innerHTML = "may I know your vehicle's name";
+
+    document.getElementById("employee-contact").style.display = "none";
+    document.getElementById("next1").style.display = "none";
+    
+    document.getElementById("button-back").style.display = "none";
+    document.getElementById("vehicle-name").style.display = "block";
+    document.getElementById("veh-name").focus();
+    document.getElementById("veh-button-next").style.display = "block";
+    
+    currentField = "vehName";
+    currentForm = 2;
+}
+
+
+/* switch case for vehicle  form for switching input fields */
+function onBlurForm2() {
+
+    switch (currentField) {
+
+        case "vehName":
+            var vehicleName = document.getElementById("veh-name").value;
+
+            if(vehicleName == ""){
+                document.getElementById("error").innerHTML="please enter a valid vehicle name";
+                return;
+            }
+            document.getElementById("error").innerHTML=" ";
+
+            label.innerHTML = " whats the type of " + vehicleName;
+
+            document.getElementById("vehicle-name").style.display = "none";
+            document.getElementById("vehicle-type").style.display = "block";
+            document.getElementById("veh-button-back").style.display = "block";
+            
+            currentField = "vehType";
+            break;
+
+        case "vehType":
+            vehicleType = document.getElementById("veh-type").value;
+            
+            if(vehicleType == ""){
+                document.getElementById("error").innerHTML="please select a vehicle type";
+                return;
+            }
+            document.getElementById("error").innerHTML=" ";
+
+            label.innerHTML = "Mention the number of " + vehicleName;
+
+            document.getElementById("vehicle-type").style.display = "none";
+            document.getElementById("vehicle-number").style.display = "block";
+            document.getElementById("veh-number").focus();
+            
+            currentField = "vehNumber";
+            break;
+
+        case "vehNumber":
+
+            var vehNumber = document.getElementById("veh-number").value;
+            
+            if(vehNumber == ""){
+                document.getElementById("error").innerHTML="please enter a valid vehicle number";
+                return;
+            }
+            document.getElementById("error").innerHTML=" ";
+
+            label.innerHTML = "Tell something about " + vehicleName;
+            
+            document.getElementById("vehicle-number").style.display = "none";
+            document.getElementById("vehicle-identification").style.display = "block";
+            document.getElementById("veh-identity").focus();
+
+            document.getElementById("next2").style.display = "block";
+            document.getElementById("veh-button-next").style.display = "none";
+
+            currentField = "vehDescription";
+            break;
+
+        default:
+    }
+}
+
+/* opens up the plan and pricing form */
+function planForm() {
+
+    document.body.style.backgroundColor = planFormBackgroundColour;
+    document.getElementById("vehicle-identification").style.display = "none";
+    document.getElementById("next2").style.display = "none";
+    document.getElementById("veh-button-back").style.display = "none";
+
+    label.innerHTML = "choose currency for your plan";
+
+    document.getElementById("select-plan").style.display = "block";
+    document.getElementById("currency-type").style.display = "block";
+    
+    currentField = "currency";
+    currentForm = 3;
+}
+
+
+/* showing available plans according to vehicle selected */
+function showPrice() {
+
+    currency = document.getElementById("currency").value;
+    plan = document.getElementById("plan").value;
+
+    document.getElementById("plan").style.display = "block";
+    
+    var vehicleType = document.getElementById("veh-type").value;
+
+    if(vehicleType == "Cycle")  {
+        var daily = 5;
+        var monthly = 100;
+        var yearly = 500;
+    }
+
+    if (vehicleType == "Motocycle") {
+        daily = 10;
+        monthly = 200;
+        yearly = 1000;
+
+    } else if (vehicleType == "Car") {
+        daily = 20;
+        monthly = 500;
+        yearly = 3500;
+    }
+
+    switch (currency) {
+        case "INR":
+            document.getElementById("daily").innerHTML = daily;
+            document.getElementById("monthly").innerHTML = monthly;
+            document.getElementById("yearly").innerHTML = yearly;
+            break;
+
+        case "USD":
+            document.getElementById("daily").innerHTML = (daily / convertToUSD).toFixed(2);
+            document.getElementById("monthly").innerHTML = (monthly / convertToUSD).toFixed(2);
+            document.getElementById("yearly").innerHTML = (yearly / convertToUSD).toFixed(2);
+            break;
+
+        case "YEN":
+            document.getElementById("daily").innerHTML = (daily / convertToYEN).toFixed(2);
+            document.getElementById("monthly").innerHTML = (monthly / convertToYEN).toFixed(2);
+            document.getElementById("yearly").innerHTML = (yearly / convertToYEN).toFixed(2);
+            break;
+    }
+
+    document.getElementById("next3").style.display = "block";
+}
+
+/* on submit of plan and pricing form */
+function submitForm() {
+
+    label.innerHTML = "Thank you for registering";
+
+    document.getElementById("main-head").style.display = "none";
+    document.getElementById("select-plan").style.display = "none";
+    document.getElementById("plan").style.display = "none";
+    document.getElementById("next3").style.display = "none";
+    document.getElementById("currency-type").style.display = "none";
+
+}
